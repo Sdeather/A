8d81313f7f2e37189a02167955583aff7f795ee0
Silks6026 <Lingl2210593848@163.com>
2026-05-13 16:01:25 +0800

    Add implementation for finding max in array
    Implement a solution to find the maximum value in an array and its index.

---
+++ Josephus-problem1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+const int MAXN=10001;
+int main(){
+	int a[MAXN];
+	int i,n,maxa,k;
+	cin>>n;
+	for(i=1;i<=n;i++) cin>>a[i];
+	maxa=a[i];
+	k=i;
+	for(i=2;i<=n;i++) 
+	if(a[i]>maxa)
+	{
+		maxa=a[i];
+		k=i;
+	}
+	cout<<k;
+	return 0;
+}
